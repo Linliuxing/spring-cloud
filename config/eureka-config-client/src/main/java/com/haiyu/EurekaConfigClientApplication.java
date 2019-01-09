db4a2c16d6afd4676e4705184b854d93b4a63b44
Radom7 <929593448@qq.com>
2019-01-09 10:50:43 +0800

    11

--- config/eureka-config-client/src/main/java/com/haiyu/EurekaConfigClientApplication.java
+++ config/eureka-config-client/src/main/java/com/haiyu/EurekaConfigClientApplication.java
@@ -2,10 +2,12 @@ package com.haiyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class EurekaConfigClientApplication {
 
 	public static void main(String[] args) {
