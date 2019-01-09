722d310533a75142432c6a173cac800d5e193006
Radom7 <929593448@qq.com>
2019-01-09 10:58:18 +0800

    11

--- config/eureka-config-client/src/main/java/com/haiyu/TestClient.java
+++ config/eureka-config-client/src/main/java/com/haiyu/TestClient.java
@@ -15,7 +15,7 @@ public class TestClient {
 	
 	public static void main(String[] args) throws Exception {
 		CloseableHttpClient client = HttpClients.createDefault();
-		HttpPost post = new HttpPost("http://localhost:8081/refresh");
+		HttpPost post = new HttpPost("http://localhost:8081/actuator/bus-refresh");
 		HttpResponse response = client.execute(post);
 		System.out.println(EntityUtils.toString(response.getEntity()));
 	}
